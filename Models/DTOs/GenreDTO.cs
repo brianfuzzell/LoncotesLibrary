6011be4c27a1fbbf68757c2e9e4822530800413d
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-06-04 15:38:17 -0500

    Project setup, models, DTOs

---
+++ Models/DTOs/GenreDTO.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoncotesLibrary.Models.DTOs;
+
+public class GenreDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ Models/DTOs/MaterialDTO.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoncotesLibrary.Models.DTOs;
+
+public class MaterialDTO
+{
+    public int Id { get; set; }
+    public string MaterialName { get; set; }
+    public int MaterialTypeId { get; set; }
+    public MaterialType MaterialType { get; set; }
+    public int GenreId { get; set; }
+    public Genre Genre { get; set; }
+    public DateTime OutOfCirculationSince { get; set; }
+}
