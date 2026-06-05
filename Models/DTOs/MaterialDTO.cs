b26ad451ef67bda1fdaa85d938a48a314fd4bd5f
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-06-05 11:40:55 -0500

    Added handler to get all materials with query params

--- Models/DTOs/MaterialDTO.cs
+++ Models/DTOs/MaterialDTO.cs
@@ -7,8 +7,8 @@ public class MaterialDTO
     public int Id { get; set; }
     public string MaterialName { get; set; }
     public int MaterialTypeId { get; set; }
-    public MaterialType MaterialType { get; set; }
+    public MaterialTypeDTO MaterialType { get; set; }
     public int GenreId { get; set; }
-    public Genre Genre { get; set; }
+    public GenreDTO Genre { get; set; }
     public DateTime? OutOfCirculationSince { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -26,4 +26,42 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/api/materials", (LoncotesLibraryDbContext db, int? materialTypeId, int? genreId) =>
+{
+    var query = db.Materials
+        .Include(m => m.Genre)
+        .Include(m => m.MaterialType)
+        .Where(m => m.OutOfCirculationSince == null);
+
+    if (materialTypeId != null)
+    {
+        query = query.Where(Material => Material.MaterialTypeId == materialTypeId);
+    }
+    if (genreId != null)
+    {
+        query = query.Where(Material => Material.GenreId == genreId);
+    }
+
+    return query
+        .Select(m => new MaterialDTO
+        {
+            Id = m.Id,
+            MaterialName = m.MaterialName,
+            MaterialTypeId = m.MaterialTypeId,
+            GenreId = m.GenreId,
+            OutOfCirculationSince = m.OutOfCirculationSince,
+            MaterialType = new MaterialTypeDTO
+            {
+                Id = m.MaterialType.Id,
+                Name = m.MaterialType.Name,
+                CheckoutDays = m.MaterialType.CheckoutDays
+            },
+            Genre = new GenreDTO
+            {
+                Id = m.Genre.Id,
+                Name = m.Genre.Name
+            }
+        }).ToList();
+});
+
 app.Run();
